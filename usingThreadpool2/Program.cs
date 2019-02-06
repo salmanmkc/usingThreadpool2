51e922b5f58d5dff8e97952180620702917737ff
SalmanMKC <32169182+salmanmkc@users.noreply.github.com>
2019-02-06 00:36:35 +0000

    doing stuff with threadpools

--- usingThreadpool2/Program.cs
+++ usingThreadpool2/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace usingThreadpool2
 {
     class Program
     {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //action means delegate call to the method with no return, func needs return
+            Task t = Task.Run(action:ThreadMethod);
+            t.Wait();
+         //   Console.ReadLine();
         }
     }
 }
